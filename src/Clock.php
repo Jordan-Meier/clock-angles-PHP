901363300f3aa0274981c6adf18ed15ead24d940
Jordan Meier and Joseph Karasek <student@epicodus.com>
2016-02-15 13:01:01 -0800

    add method to Clock, returns angle of complex hour inputs

--- src/Clock.php
+++ src/Clock.php
@@ -3,6 +3,9 @@
     {
         function hourHandAngle($hour)
         {
+            if ($hour >= 12) {
+                $hour -= 12;
+            }
             return ($hour/12)*360;
         }
     }
--- tests/ClockTest.php
+++ tests/ClockTest.php
@@ -8,12 +8,18 @@
             // arrange
             $test_Clock = new Clock;
             $input_hour = 1;
+            $input_hour2 = 12;
+            $input_hour3 = 1.5;
 
             // act
             $result = $test_Clock->hourHandAngle($input_hour);
+            $result2 = $test_Clock->hourHandAngle($input_hour2);
+            $result3 = $test_Clock->hourHandAngle($input_hour3);
 
             // assert
             $this->assertEquals(30, $result);
+            $this->assertEquals(0, $result2);
+            $this->assertEquals(45, $result3);
         }
     }
 ?>
