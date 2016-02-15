566334de4bfd98b2aa52a418cda0e142b9a27d1a
Joseph Karasek and Jordan Meier <joseph.karasek@gmail.com>
2016-02-15 13:45:25 -0800

    add a index twig file with a form and result display, as well as routes in app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,21 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Clock.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/angle", function() use ($app) {
+        $my_Clock = new Clock;
+        $results = $my_Clock->angleBetweenHands($_GET['time']);
+        return $app['twig']->render('index.html.twig', array('result' => $results));
     });
 
     return $app;
